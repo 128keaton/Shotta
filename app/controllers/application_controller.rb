61cd98b08c1ced102b23d504397cfe785de45d51
Keaton Burleson <keaton.burleson@me.com>
2017-08-16 15:16:29 -0500

    Moved to Postgres

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   protect_from_forgery with: :exception
+  before_action :detect_browser
+  def detect_browser
+    case request.user_agent
+    when /iPhone/i
+      request.variant = :phone
+      request.format = :json unless request.path.include?(Devise.mappings[:user].fullpath) || user_signed_in?
+    else
+      request.variant = :desktop
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   post 'screenshots/upload'
   root to: "screenshots#index"
   
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'sessions', registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
