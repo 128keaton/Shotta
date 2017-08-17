6ccaaf6c44b7ca814e3d22a9c0ae214295ec3e94
Keaton Burleson <keaton.burleson@me.com>
2017-08-16 19:07:18 -0500

    Added all social metas

--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -1,5 +1,29 @@
 class ViewerController < ApplicationController
+  include ActionView::Helpers::TagHelper
+  
   def index
       @screenshot =  Screenshot.find(params[:id])
+      @meta = social_meta_tags(@screenshot)
+  end
+
+  def social_meta_tags(screenshot)
+    title = screenshot.created_at
+    description = screenshot.image_identifier
+    image = screenshot.image
+
+    content_tag(:title, title) +
+    content_tag(:meta, nil, content: title, property: 'og:title') +
+    content_tag(:meta, nil, content: description, property: 'og:description') +
+    content_tag(:meta, nil, content: image, property: 'og:image') +
+    content_tag(:meta, nil, content: title, name: 'title') +
+    content_tag(:meta, nil, content: description, name: 'description') +
+    content_tag(:link, nil, href: image, rel: 'image_src').html_safe +
+    content_tag(:meta, nil, content: 'twitter summary', name: 'twitter:card') +
+    content_tag(:meta, nil, content: title, name: 'twitter:title') +
+    content_tag(:meta, nil, content: description, name: 'twitter:description') +
+    content_tag(:meta, nil, content: image, name: 'twitter:image') +
+    content_tag(:meta, nil, content: title, itemprop: 'name') +
+    content_tag(:meta, nil, content: description, itemprop: 'description') +
+    content_tag(:meta, nil, content: image, itemprop: 'image')
   end
 end
